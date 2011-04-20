08d2224e31e230e9c810aa9f7416107a78f3d575
Edward Longman <eialongman@gmail.com>
2011-04-21 00:13:24 +0100

    has reference to box, has function on initiation (writeAll), and a function for when its pinged (write), both loop to a function that writes individual and deletes od ones so there are no more than 14

---
+++ js/write.js
@@ -0,0 +1,30 @@
+shoutbox_box=document.getElementById('shouts');
+function writeAll(){
+	shouts=amplify.store('shouts');
+	for(var i=shouts.length-1;i>=0;i--){
+		writeShout(shouts[i]);
+	}
+}
+function write(newShouts){
+	shouts=amplify.store('shouts');
+	for(var i=newShouts.length-1;i>=0;i--){
+		writeShout(newShouts[i]);
+	}
+}
+function writeShout(shout_details){
+	shoutbox_box.innerHTML+='\
+        <li class="' + shout_details.group + '">\
+          <header>\
+            <h6>' + shout_details.member + ' <span class="right">(' + shout_details.group + ')</span></h6>\
+            <br class="clearfix" />\
+          </header>\
+          <p>' + shout_details.message + '</p>\
+          <footer>\
+            <em class="right">' + shout_details.date + '</em>\
+          </footer>\
+          <br class="clearfix" />\
+        </li>';
+	if(shoutbox_box.children.length>15){
+		shoutbox_box.children[0].removeNode();
+	}
+}
