ed295f570325745150a2ada5cef0d91d170de0fb
Edward Longman <eialongman@gmail.com>
2011-04-21 11:25:32 +0100

    removed clearTimeouts

--- js/ajax.js
+++ js/ajax.js
@@ -3,7 +3,6 @@ localStorage.setItem('shouts','[]');
 Shoutbox.reload=function(){
 	var messid=(shouts.length==0)?0:shouts[shouts.length-1].id
 	Shoutbox.getShouts(messid);
-	if(timer!=undefined)clearTimeout(timer);
 	timer=setTimeout('Shoutbox.reload()',reloadTime*5);
 }
 Shoutbox.getShouts=function(lm){
@@ -21,7 +20,6 @@ Shoutbox.output_shouts=function(got_shouts){
 	stringified=JSON.stringify(shouts);
 	localStorage.setItem('shouts',stringified);
 	chrome.extension.sendRequest(got_shouts);
-	if(timer!=undefined)clearTimeout(timer);
 	timer=setTimeout('Shoutbox.reload()',reloadTime);
 }
 Shoutbox.reload();
