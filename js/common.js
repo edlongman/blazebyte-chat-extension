d7120c5f0b09b4fa642667e3b2db046b18bb7aa8
Edward Longman <eialongman@gmail.com>
2011-04-21 10:40:55 +0100

    now parses shouts onload

--- js/common.js
+++ js/common.js
@@ -3,7 +3,7 @@ if(!localStorage.getItem('run')){
 	localStorage.setItem('run',true);
 }
 Shoutbox={};
-shouts=localStorage.getItem('shouts');
+shouts=JSON.parse(localStorage.getItem('shouts'));
 popup={width:200,height:390};
 reloadTime=3500
 max_messages=15;
