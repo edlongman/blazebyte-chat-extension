4ea2e2fadf2bb933964d1a390c0f01fde08120fc
Edward Longman <eialongman@gmail.com>
2011-04-24 15:11:05 +0100

    moved all shoutbox objects into a object.

--- js/ajax.js
+++ js/ajax.js
@@ -1,32 +1,34 @@
 shouts=[];
 localStorage.setItem('shouts','[]');
-Shoutbox.reload=function(){
-	shouts=JSON.parse(localStorage.getItem('shouts'))
-	var messid=(shouts.length==0)?0:shouts[0].id
-	Shoutbox.getShouts(messid);
-	timer=setTimeout('Shoutbox.reload()',reloadTime);
-}
-Shoutbox.getShouts=function(lm){
-	var head = document.head;
-    var script = document.createElement("script");
-	var url="http://blazebyte.org/shoutbox/shoutbox.php?lu="+lm+'&_='+(new Date()).getTime();
-	script.src=url;
-	script.onload=function(){
-		head.removeChild(script);
-	}
-	head.appendChild(script);
-}
-Shoutbox.output_shouts=function(got_shouts){
-	shouts=got_shouts.concat(shouts);
-	while(shouts.length>15)shouts.pop();
-	stringified=JSON.stringify(shouts);
-	localStorage.setItem('shouts',stringified);
-	chrome.extension.sendRequest(got_shouts);
-	if(localStorage.getItem('iconState')!='stop'){
-		flashIcon(true);
-		tryChime();
-		//animateFlip(0,false);
-		localStorage.setItem('iconState','alert');
+Shoutbox={
+	reload:function(){
+		shouts=JSON.parse(localStorage.getItem('shouts'))
+		var messid=(shouts.length==0)?0:shouts[0].id
+		Shoutbox.getShouts(messid);
+		timer=setTimeout('Shoutbox.reload()',reloadTime);
+	},
+	getShouts:function(lm){
+		var head = document.head;
+	    var script = document.createElement("script");
+		var url="http://blazebyte.org/shoutbox/shoutbox.php?lu="+lm+'&_='+(new Date()).getTime();
+		script.src=url;
+		script.onload=function(){
+			head.removeChild(script);
+		}
+		head.appendChild(script);
+	},
+	output_shouts:function(got_shouts){
+		shouts=got_shouts.concat(shouts);
+		while(shouts.length>15)shouts.pop();
+		stringified=JSON.stringify(shouts);
+		localStorage.setItem('shouts',stringified);
+		chrome.extension.sendRequest(got_shouts);
+		if(localStorage.getItem('iconState')!='stop'){
+			flashIcon(true);
+			tryChime();
+			//animateFlip(0,false);
+			localStorage.setItem('iconState','alert');
+		}
 	}
 }
 Shoutbox.reload();
